82dbfeb096111935da5986210576446020a6f89b
Olesia Guidi <olgarbuzova@gmail.com>
2020-03-05 12:11:14 -0500

    app is completed

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+
 @Component({
 	selector: 'app-hero-detail',
 	templateUrl: './hero-detail.component.html',
 	styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-	constructor() {}
-  @Input() hero: Hero;
-	ngOnInit(): void {}
+	constructor(
+		private route: ActivatedRoute,
+		private heroService: HeroService,
+		private location: Location
+	) {}
+	@Input() hero: Hero;
+	ngOnInit(): void {
+		this.getHero();
+	}
+	getHero(): void {
+    //route.snapshot is a static image of the route info
+		const id = +this.route.snapshot.paramMap.get('id');
+		this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+  }
+  goBack(): void {
+    this.location.back();
+  }
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
 	selector: 'app-heroes',
 	templateUrl: './heroes.component.html',
 	styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-	constructor() {}
-	hero: Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
-  heroes = HEROES;
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-	ngOnInit(): void {}
+	constructor(private heroService: HeroService) {}
+
+	heroes: Hero[];
+
+	ngOnInit() {
+		this.getHeroes();
+	}
+
+	//async approach, when heroes received, subscribe() will pass array to the callback
+	getHeroes(): void {
+		this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+	}
+	add(name: string): void {
+		name = name.trim();
+		if (!name) {
+			return;
+		}
+		this.heroService.addHero({ name } as Hero).subscribe(hero => {
+			this.heroes.push(hero);
+		});
+	}
+	delete(hero: Hero): void {
+		this.heroes = this.heroes.filter(h => h !== hero);
+		this.heroService.deleteHero(hero).subscribe();
+	}
 }
